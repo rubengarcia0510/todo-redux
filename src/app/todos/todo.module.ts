24deae93d897090c45c8a8d37c41ff6a40d19443
Ruben Garcia <garcia.r@opendevpro.com>
2023-05-31 18:30:42 -0300

    add todo-item

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoAddComponent } from './todo-add/todo-add.component';
 import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 
 
 @NgModule({
   declarations: [
     TodoAddComponent,
-    TodoFooterComponent
+    TodoFooterComponent,
+    TodoItemComponent
   ],
   imports: [
     CommonModule

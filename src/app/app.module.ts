b756adbb10d4b965adcd4bbd3fd69b2502fcd41c
Ruben Garcia <garcia.r@opendevpro.com>
2023-05-31 18:24:55 -0300

    creo modulo todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]

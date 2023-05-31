fd89c9d0b91854f4a16ab6001d2829ea4ea21d34
Ruben Garcia <garcia.r@opendevpro.com>
2023-05-31 18:32:45 -0300

    add todo-page

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -4,6 +4,7 @@ import { TodoAddComponent } from './todo-add/todo-add.component';
 import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
 
 
 
@@ -12,7 +13,8 @@ import { TodoListComponent } from './todo-list/todo-list.component';
     TodoAddComponent,
     TodoFooterComponent,
     TodoItemComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoPageComponent
   ],
   imports: [
     CommonModule

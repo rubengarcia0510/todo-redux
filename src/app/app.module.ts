3bfc1dfc223890b441ea07aaa9d28b4bf8dd6fd8
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-01 09:53:10 -0300

    after upgrade to 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { FooterComponent } from './footer/footer.component';
 
 import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,8 @@ import { todoReducer } from './todos/todo.reducer';
   imports: [
     BrowserModule,
     TodoModule,
-    StoreModule.forRoot({todos:todoReducer})
+    StoreModule.forRoot({todos:todoReducer}),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]

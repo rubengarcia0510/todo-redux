cbf9771caa418d9ff9144936151f77ffeea5379c
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-01 15:18:21 -0300

    modificar para tomar valores de la lista

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo:Todo;
+  @ViewChild('inputFisico') textInputFisico:ElementRef;
+
+  chkCompletado: FormControl;
+  inputText: FormControl;
+  editando:boolean=false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(
+  ): void {
+
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.inputText = new FormControl(this.todo.texto,Validators.required);
+
+
+  }
+
+  editar(){
+    this.editando=true;
+
+
+    setTimeout(()=>{
+      this.textInputFisico.nativeElement.select();
+    },1)
+  }
+
+  update(){
+    this.editando=false;
   }
 
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -3,7 +3,11 @@ import { add } from './todo.actions';
 import { Todo } from './models/todo.model';
 
 
-export const initialState:Todo[] = [];
+export const initialState:Todo[] = [
+  new Todo("Salvar al mundo"),
+  new Todo("Vencer a Thanos"),
+  new Todo("Robar escudo del Capitan America"),
+];
 
 const _todoReducer = createReducer(
   initialState,
